252db297dc511ce29a7ab127ae817f7de9df2734
LEANDRO ANRIQUEZ <99001904+gonzalezanriquez@users.noreply.github.com>
2026-04-30 13:07:01 -0300

    up

--- api/index.php
+++ api/index.php
@@ -1,18 +1,10 @@
+<?php
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+require_once __DIR__ . '/saludo.php';
 
-<?php include("saludo.php")
+header('Content-Type: text/html');
 
+echo "<h1>" . hola() . "</h1>";
 
 
 ?>
-    <h1> <?php echo hola() ?> </h1>
-</body>
-</html>
